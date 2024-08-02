ce55b34c560eafa3414f552d72159718818f364e
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-02 14:27:15 +0530

    User DAO created

--- composeApp/src/commonMain/kotlin/core/data/dao/UserDao.kt
+++ composeApp/src/commonMain/kotlin/core/data/dao/UserDao.kt
@@ -1,4 +1,20 @@
 package core.data.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import core.data.entity.User
+
+@Dao
 interface UserDao {
+
+    // For login
+    @Query("SELECT * FROM User WHERE email = :email")
+    fun gerUserByEmail(email:String): User
+
+    // For registering
+    @Upsert
+    fun registerUser(user: User)
+
+
 }
