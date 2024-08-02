de2e8275a459eaea5e9142419cb9753bf74f9ead
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-02 16:06:14 +0530

    Database get funtion for android target added in android main

--- composeApp/src/androidMain/kotlin/core/data/database/getDatabase.kt
+++ composeApp/src/androidMain/kotlin/core/data/database/getDatabase.kt
@@ -1,2 +1,18 @@
 package core.data.database
 
+import android.content.Context
+import androidx.room.Room
+import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+
+
+fun getDataBase(context: Context):Database{
+
+    val dbFile = context.getDatabasePath("database.db")
+
+    return  Room.databaseBuilder<Database>(
+        context = context,
+        name = dbFile.absolutePath
+    )
+        .setDriver(BundledSQLiteDriver())
+        .build()
+}
