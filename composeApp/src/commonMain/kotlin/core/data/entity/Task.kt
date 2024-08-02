19308327db7d9df956573f6a14a4ba8adf253c37
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-02 15:10:56 +0530

    Task Entity added

--- composeApp/src/commonMain/kotlin/core/data/entity/Task.kt
+++ composeApp/src/commonMain/kotlin/core/data/entity/Task.kt
@@ -1,3 +1,16 @@
 package core.data.entity
 
-data class Task()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.datetime.LocalDate
+
+
+@Entity
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int = 0,
+    val title:String,
+    val description:String,
+    val dueDate:LocalDate,
+    val status:Boolean
+)
