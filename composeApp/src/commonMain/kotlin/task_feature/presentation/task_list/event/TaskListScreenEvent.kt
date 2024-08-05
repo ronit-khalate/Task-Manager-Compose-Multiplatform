feaea63cdc6ab731e2bd80aa57489e7c3e682ee6
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 02:02:22 +0530

    TaskList Event Created

---
+++ composeApp/src/commonMain/kotlin/task_feature/presentation/task_list/event/TaskListScreenEvent.kt
@@ -0,0 +1,17 @@
+package task_feature.presentation.task_list.event
+
+import task_feature.domain.TaskFilter
+
+sealed interface TaskListScreenEvent {
+
+    data object OnSearchedButtonClicked:TaskListScreenEvent
+    data object OnAscendingButtonClicked:TaskListScreenEvent
+    data object OnDescendingButtonClicked:TaskListScreenEvent
+    data object OnFilterButtonChanged:TaskListScreenEvent
+    data object OnAddTaskButtonClicked:TaskListScreenEvent
+
+    data class OnFilterTypeChose(val filter:TaskFilter):TaskListScreenEvent
+    data class OnSearchTextEntered(val text:String):TaskListScreenEvent
+
+
+}
