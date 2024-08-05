d9ace65a304a2f39b8be423ef947157ad2067325
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 01:35:46 +0530

    Login repository dependency added app module

--- composeApp/src/commonMain/kotlin/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/di/AppModule.kt
@@ -1,6 +1,8 @@
 package di
 
+import auth.data.repository.LoginRepositoryImpl
 import auth.data.repository.RegistrationRepositoryImp
+import auth.domain.repository.LoginRepository
 import auth.domain.repository.RegistrationRepository
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -9,8 +11,12 @@ import org.koin.dsl.module
 val appModule = module {
 
         single {
-
             RegistrationRepositoryImp(get())
-        }
+
+        }.bind(RegistrationRepository::class)
+
+        single {
+            LoginRepositoryImpl(get())
+        }.bind(LoginRepository::class)
 
 }
