08d355f8ca3521be1889d911ac9a41cf1a245150
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 02:28:05 +0530

    Task enitity and TaskDto mapper added

--- composeApp/src/commonMain/kotlin/core/data/mapper/Mappers.kt
+++ composeApp/src/commonMain/kotlin/core/data/mapper/Mappers.kt
@@ -1,7 +1,9 @@
 package core.data.mapper
 
 import auth.domain.UserDto
+import core.data.entity.Task
 import core.data.entity.User
+import task_feature.domain.TaskDto
 
 
 fun User.toUserDto():UserDto = UserDto(
@@ -15,4 +17,21 @@ fun UserDto.toUser():User = User(
     name = this.name,
     email = this.email,
     password = this.password
+)
+
+fun Task.toTaskDto():TaskDto = TaskDto(
+    id = this.id,
+    title=this.title,
+    description = this.description,
+    dueDate = this.dueDate,
+    status = this.status
+)
+
+
+fun TaskDto.toTask():Task = Task(
+    id = this.id,
+    title=this.title,
+    description = this.description,
+    dueDate = this.dueDate,
+    status = this.status
 )
