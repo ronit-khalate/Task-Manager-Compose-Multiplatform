ec15835e749bb9f18351ccd5f213c313bff7c8e0
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 01:37:38 +0530

    Login repository dependency added destops viewmodel module

--- composeApp/src/desktopMain/kotlin/di/viewModelModule.kt
+++ composeApp/src/desktopMain/kotlin/di/viewModelModule.kt
@@ -1,11 +1,23 @@
 package di
+import auth.data.repository.LoginRepositoryImpl
+import auth.domain.repository.LoginRepository
+import auth.domain.repository.RegistrationRepository
+import auth.presentation.login.LoginScreenViewModel
 import auth.presentation.register.RegisterViewModel
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 actual val viewModelModule = module {
 
     single{
         RegisterViewModel(get())
+
     }
+
+    single {
+
+
+        LoginRepositoryImpl(get())
+    }.bind(LoginRepository::class)
 }
