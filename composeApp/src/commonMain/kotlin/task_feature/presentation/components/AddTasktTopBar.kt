531f6f3e3e8d5ecbd9ecb8935731700ab85d9dd9
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 01:33:23 +0530

    top bar for addTask screen created

---
+++ composeApp/src/commonMain/kotlin/task_feature/presentation/components/AddTasktTopBar.kt
@@ -0,0 +1,74 @@
+package task_feature.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import compose.icons.TablerIcons
+import compose.icons.tablericons.Bold
+import compose.icons.tablericons.Filter
+import compose.icons.tablericons.SortAscending
+import compose.icons.tablericons.SortDescending
+
+
+@Composable
+fun AddTaskTopBar(
+    modifier: Modifier = Modifier,
+    innerPadding:Dp
+) {
+
+    Column(
+        modifier = modifier
+            .padding(start = innerPadding, end = innerPadding-10.dp, top = innerPadding)
+            .fillMaxWidth()
+            .height(128.dp)
+
+            .background(Color(0xFF27323A)),
+        verticalArrangement = Arrangement.Top
+    ){
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.Top
+        ) {
+            Text(
+                text = "Task",
+                fontSize = 28.sp,
+                fontWeight = FontWeight(700),
+                color = Color.LightGray
+
+            )
+
+
+        }
+
+
+    }
+}
