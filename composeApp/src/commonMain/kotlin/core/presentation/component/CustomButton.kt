1d056626283fcf80ee8e8b6b86831ef4a0cee0c3
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 01:30:51 +0530

    custom text filed and button created

---
+++ composeApp/src/commonMain/kotlin/core/presentation/component/CustomButton.kt
@@ -0,0 +1,40 @@
+package core.presentation.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomButton(
+    modifier: Modifier = Modifier
+        .height(64.dp)
+        .fillMaxWidth(),
+    onClick:()->Unit,
+    buttonText:String,
+) {
+
+
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color(0xFF29A19C), contentColor = Color(0xFFFFFFFF)),
+
+        ){
+        Text(
+            text = buttonText,
+            fontWeight = FontWeight(700),
+            fontSize = 20.sp
+        )
+    }
+
+}
---
+++ composeApp/src/commonMain/kotlin/core/presentation/component/CustomTextField.kt
@@ -0,0 +1,80 @@
+package core.presentation.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun CustomTextField(
+    modifier: Modifier = Modifier,
+    value:String,
+    onValueChange:(String)->Unit,
+    placeHolder:String
+) {
+
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .clip(RoundedCornerShape(10.dp)),
+        shape = RoundedCornerShape(10.dp),
+        backgroundColor = Color(0xFF435055)
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.Start
+        ) {
+
+
+            BasicTextField(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(start = 24.dp),
+                value = value,
+                onValueChange = onValueChange,
+                textStyle = TextStyle(
+                    color = Color(0xFFFFFFFF),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight(700)
+                )
+
+
+                ) {
+
+                if (value.isBlank()) {
+                    Text(
+                        text = placeHolder,
+                        color = Color.LightGray,
+                        fontWeight = FontWeight(700),
+                        fontSize = 20.sp
+
+                    )
+                }
+
+                it()
+            }
+        }
+    }
+    
+}
