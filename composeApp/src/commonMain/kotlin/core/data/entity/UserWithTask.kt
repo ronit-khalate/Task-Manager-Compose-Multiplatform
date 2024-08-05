db4dd42a8ae489b207ce9320a9a7b84b21ff88d7
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 02:52:06 +0530

    Created UserWithTask to get all task of perticuler user

---
+++ composeApp/src/commonMain/kotlin/core/data/entity/UserWithTask.kt
@@ -0,0 +1,13 @@
+package core.data.entity
+
+import androidx.room.Embedded
+import androidx.room.Relation
+
+data class UserWithTask(
+    @Embedded val user: User,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "userId"
+    )
+    val tasks:List<Task>
+)
