5f5a02a87e7101baacb61bf5288cb17944bfce09
ronit-khalate <ronitkhalate243@gmail.com>
2024-08-06 02:51:23 +0530

    Added User Id As ForeginKey for many to one relation

--- composeApp/src/commonMain/kotlin/core/data/entity/Task.kt
+++ composeApp/src/commonMain/kotlin/core/data/entity/Task.kt
@@ -1,16 +1,30 @@
 package core.data.entity
 
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import androidx.room.util.TableInfo
 import kotlinx.datetime.LocalDate
 
 
-@Entity
+@Entity(
+    foreignKeys = [
+       ForeignKey(
+           entity = User::class,
+           parentColumns = arrayOf("id"),
+           childColumns = arrayOf("userId"),
+           onDelete = ForeignKey.CASCADE
+       ),
+    ],
+        indices = [Index(value = ["userId"])]
+)
 data class Task(
     @PrimaryKey(autoGenerate = true)
     val id:Int = 0,
     val title:String,
     val description:String,
     val dueDate:String,
-    val status:Boolean=false
+    val status:Boolean=false,
+    val userId:Int
 )
